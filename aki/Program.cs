6b7b5a6c8bbeccaa442c8beaf19ba5f8378f538e
Сhert <hert@AKIRIN>
2022-10-01 10:47:33 +0300

    Добавьте файлы проекта.

---
+++ aki/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+    namespace aki
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Полученное число: ");
+            int n = int.Parse(Console.ReadLine());
+            int a = n / 100;
+            int c = (n / 10) % 10;
+            int b = n % 10;
+            int x = 100 * a + 10 * b + c;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+     
+    }
+
+}
+
